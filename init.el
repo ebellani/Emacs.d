44c40d7e61fc0c9d99e6e5c08317018dcee2ad7c
Eduardo Bellani <ebellani@gmail.com>
2020-09-01 20:22:46 -0300

    Edit server

--- init.el
+++ init.el
@@ -957,6 +957,23 @@ hit C-a twice:"
   :bind (:map pyenv-mode-map
               ("C-c C-s" . nil)))
 
+(use-package edit-server
+  :ensure t
+  :commands edit-server-start
+  :init (if after-init-time
+              (edit-server-start)
+            (add-hook 'after-init-hook
+                      #'(lambda() (edit-server-start))))
+  :config (setq edit-server-new-frame-alist
+                '((name . "Edit with Emacs FRAME")
+                  (top . 200)
+                  (left . 200)
+                  (width . 80)
+                  (height . 25)
+                  (minibuffer . t)
+                  (menu-bar-lines . t)
+                  (window-system . x))))
+
 (setq use-package-always-ensure nil)
 
 (put 'scroll-left 'disabled nil)
