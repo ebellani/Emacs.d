6f5f3a26707564c2a816fac879bdd448a82b3c03
Eduardo Bellani <ebellani@gmail.com>
2020-09-28 08:52:50 -0300

    Add flex

--- init.el
+++ init.el
@@ -95,6 +95,11 @@ accumulating.")
   :config
   (scroll-bar-mode 0))
 
+(use-package minibuffer
+  :config
+  (setq completion-styles '(flex))
+  (scroll-bar-mode 0))
+
 (use-package gnu-elpa-keyring-update
   :ensure t)
 
@@ -260,7 +265,6 @@ http://www.howardism.org/Technical/Emacs/eshell-fun.html"
       (other-window 1)
       (eshell "new")
       (rename-buffer (concat "*eshell: " name "*"))
-
       (insert (concat "ls"))
       (eshell-send-input)))
   (defun eshell/c ()
@@ -286,11 +290,18 @@ hit C-a twice:"
   (setenv "PAGER" "cat")
   (setq eshell-history-size 1024
         eshell-visual-commands
-        '("mtr -t" "nethogs"  "htop" "ncdu" "nmon"
+        '("mtr" "nethogs"  "htop" "ncdu" "nmon"
           "vi" "screen" "top" "less" "more" "lynx"
           "ncftp" "pine" "tin" "trn" "elm"))
-  :bind  (("C-a"   . 'eshell-maybe-bol)
-          ("C-x !" . 'eshell-here)))
+  :bind  (:map
+          global-map
+          ("C-x !" . 'eshell-here)
+          :map
+          eshell-command-map
+          ("C-a" . eshell-maybe-bol)
+          :map
+          eshell-mode-map
+          ("C-a" . eshell-maybe-bol)))
 
 (use-package ispell
   :no-require t
@@ -787,11 +798,10 @@ hit C-a twice:"
         helm-buffer-max-length                 nil
         helm-show-completion-display-function #'helm-show-completion-default-display-function
         ;; the following would enable a separate frame.
-        ;; helm-display-function                  'helm-display-buffer-in-own-frame
-        ;; helm-display-buffer-reuse-frame        t
-        ;; helm-use-undecorated-frame-option      t
-        ;; helm-display-buffer-width  120
-        )
+        helm-display-function                  'helm-display-buffer-in-own-frame
+        helm-display-buffer-reuse-frame        t
+        helm-use-undecorated-frame-option      t
+        helm-display-buffer-width  120)
   (helm-mode 1)
   (helm-adaptive-mode 1)
   (add-hook 'eshell-mode-hook
