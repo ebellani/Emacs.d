65114a9de1cb6ec2ed4af3d5b14f0bfbca4c73df
Eduardo Bellani <ebellani@gmail.com>
2020-12-05 20:42:39 -0300

    Add ediff function

--- init.el
+++ init.el
@@ -202,7 +202,17 @@ accumulating.")
 
 (use-package ediff
   ;; https://emacs.stackexchange.com/a/21336/16861
-  :config (add-hook 'ediff-prepare-buffer-hook #'show-all))
+  :config
+  (add-hook 'ediff-prepare-buffer-hook #'show-all)
+  ;; https://stackoverflow.com/a/29757750
+  (defun ediff-copy-both-to-C ()
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+                     (concat
+                      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+  (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map))
 
 (use-package comint
   ;; This is based on
