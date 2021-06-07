6c7f1569126f211a16a9368c1854ab1b46e485b9
Eduardo Bellani <eduardo.bellani@datarisk.io>
2021-06-06 22:38:50 -0300

    add jz tempo

--- init.el
+++ init.el
@@ -726,7 +726,24 @@ hit C-a twice:"
         `((".*" ,temporary-file-directory t))))
 
 (use-package org-tempo
-  :after org)
+  :after org
+  :config
+  (tempo-define-template "org-drill-1"
+		         '("* Item :drill:\n<QUESTION>\n\n** Answer\n<ANSWER>")
+		         "<1"
+		         "Template for and org drill's simple topic")
+  (tempo-define-template "org-drill-2"
+		         '("* Item :drill:\n:PROPERTIES:\n:DRILL_CARD_TYPE: twosided\n:END:\n\n<QUESTION>\n\n** Side A\n\n** Side B\n")
+		         "<2"
+		         "Template for an org drills' 2 sided card")
+  (tempo-define-template "org-drill-3"
+		         '("* Item :drill:\n:PROPERTIES:\n:DRILL_CARD_TYPE: hide1cloze\n:END:\n\n<QUESTION>\n")
+		         "<3"
+		         "Template for an org drills' hide1cloze card")
+  (tempo-define-template "org-drill-4"
+		         '("* Item :drill:\n")
+		         "<4"
+		         "Simples org dril template"))
 
 (use-package ox
   :after org
