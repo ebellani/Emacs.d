051ad78ffa904181b6354067c2cbe2ce9c9eb352
Eduardo Bellani <eduardo.bellani@datarisk.io>
2022-02-12 21:11:45 -0300

    fix: init for elfeed

--- init.el
+++ init.el
@@ -1176,8 +1176,8 @@ hit C-a twice:"
 (use-package elfeed
   :straight t
   :bind (("C-x w" . 'elfeed))
-  :config (setq elfeed-search-title-max-width 140
-                elfeed-show-entry-switch #'pop-to-buffer))
+  :init (setq elfeed-search-title-max-width 140
+              elfeed-show-entry-switch #'pop-to-buffer))
 
 
 (use-package elfeed-org
